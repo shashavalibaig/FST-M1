d432305d272f9e3c158008f1b10103ef7bea6723
shashavalibaig <74968808+shashavalibaig@users.noreply.github.com>
2022-04-06 15:04:12 +0530

    Add files via upload
    SQL Activity 1

---
+++ SQL/Activities/sqlscript-activity-1.sql
@@ -0,0 +1,10 @@
+REM   Script: Activity-1
+REM   SQL Activity-1
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
